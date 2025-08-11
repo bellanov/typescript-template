3bdc4e6b62fd36c6408ebed178554ba7eb7be4a5
Bellanov <bellanovapilli@gmail.com>
2025-08-10 20:33:23 -0400

    Initial Classes Integration

--- src/classes.ts
+++ src/classes.ts
@@ -5,6 +5,27 @@
  */
 
 namespace Classes {
-  
+
+  export class Vehicle {
+    wheels: number;
+    power: number;
+    speed: number = 0;
+
+    constructor(wheels: number, power: number) {
+      this.wheels = wheels;
+      this.power = power;
+    }
+
+    accelerate(time: number): void {
+        this.speed = this.speed + 0.5 * this.power * time;
+    }
+  }
 }
 
+// Import class exported from namespace
+const myVehicle: Classes.Vehicle = new Classes.Vehicle(4, 150);
+myVehicle.accelerate(10);
+console.log(`My vehicle's speed is ${myVehicle.speed} km/h.`);
+
+// Replace content within HTML
+document.getElementById("vehicle-speed")!.textContent = `${myVehicle.speed} km/h.`;

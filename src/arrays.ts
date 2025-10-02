2b5fa2982913789677dc4332f251a2b7f227f7dc
Bellanov <bellanovapilli@gmail.com>
2025-10-02 12:58:06 -0400

    Refactored Project
    Refactored the project to better adhere to proper file patterns and structure.

---
+++ src/arrays.ts
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview Introduction to TypeScript. This example demonstrates basic
+ * TypeScript features such as types, interfaces, and functions.
+ */
+
+namespace Arrays {
+  // An array of numbers - Method 1
+  // Angle brackets - familiar to C# and Java developers
+  export let list1: Array<number> = [1, 2, 3, 4, 5];
+
+  // An array of numbers - Method 2
+  // Square brackets - more common in TypeScript and familiar to developers.
+  export let list2: number[] = [6, 7, 8, 9, 10];
+}
+
+// Push a number to a list
+// Appends new elements to the end of an array, and returns the new length 
+// of the array.
+Arrays.list1.push(6);
+
+// Pop a number from a list
+// Removes the last element from an array and returns that element.
+Arrays.list1.pop();
+
+// Unshift a number to the beginning of a list
+// Adds new elements to the beginning of an array and returns the new length
+// of the array.
+Arrays.list1.unshift(0);
+
+// Shift a number from the beginning of a list
+// Removes the first element from an array and returns that element.
+Arrays.list1.shift();
+
+document.getElementById("array-list-1")!.textContent = Arrays.list1.toString();
+document.getElementById("array-list-2")!.textContent = Arrays.list2.toString();
+
